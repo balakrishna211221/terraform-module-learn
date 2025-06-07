35282182f23afd6dcc8e6a26f000e0dd8a688d22
balakrishna211221 <37782387+balakrishna211221@users.noreply.github.com>
2025-06-07 18:06:18 +0000

    added network, igw, routetable, routes

--- network.tf
+++ network.tf
@@ -16,3 +16,28 @@ module "subnet" {
     Name = "MySubnet"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = module.vpc.vpc_id
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = module.vpc.vpc_id
+  tags = {
+    Name = "MyRouteTable"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "subnet_association" {
+  subnet_id      = module.subnet.subnet_id
+  route_table_id = aws_route_table.route_table.id
+}
