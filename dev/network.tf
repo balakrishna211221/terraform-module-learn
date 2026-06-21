1becc6e0cfd3a49a47e2a13373032373d448b947
balakrishna211221 <balubharath.a@gmail.com>
2026-06-22 00:07:04 +0530

    fix: update VPC module name variable to use correct reference

--- dev/network.tf
+++ dev/network.tf
@@ -1,6 +1,6 @@
 module "application_vpc" {
         source = "../modules/networking/vpc"
-        name = var.myterraform_vpc
+        name = var.name
         environment = var.environment
         vpc_cidr = var.vpc_cidr
         private_subnet_cidr = var.private_subnet_cidr
